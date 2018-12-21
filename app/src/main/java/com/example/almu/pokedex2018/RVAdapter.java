8239a3513a82a91251ad2b78f1c3709c04204c06
AlmuChan <39128933+AlmuChan@users.noreply.github.com>
2018-12-21 14:03:32 +0100

    ErrorSolucionado Ahora Visual
    El pokemon 1 no se muestra en la app

--- app/src/main/java/com/example/almu/pokedex2018/RVAdapter.java
+++ app/src/main/java/com/example/almu/pokedex2018/RVAdapter.java
@@ -1,78 +1,77 @@
 package com.example.almu.pokedex2018;
 
-import android.support.v7.widget.RecyclerView;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
 import java.util.List;
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.PokemonViewHolder> {
-    List<Pokemon> pokemonList;
-    RecyclerView recycler;
-
-    public RVAdapter(List<Pokemon> pokemonList) {
-        this.pokemonList = pokemonList;
-    }
-
-    //para que queremos saber el tamaño de la lista?
-    @Override
-    public int getItemCount() {
-        return pokemonList.size();
-    }
+public class RVAdapter  extends RecyclerView.Adapter<RVAdapter.AnimeViewHolder>{
+    private List<Pokemon> items;
+    public RecyclerView recycler;
 
     @Override
-    public PokemonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.content_main,
-                viewGroup, false);
-        return new PokemonViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(PokemonViewHolder pokemonViewHolder, int i) {
-        Glide.with(pokemonViewHolder.pokemonFoto.getContext())
-                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokemonList.get(i).id+".png")
-                //.placeholder(R.mipmap.ic_launcher_round)
-                .into(pokemonViewHolder.pokemonFoto);
-
-        pokemonViewHolder.pokemonNombre.setText(pokemonList.get(i).id+".-"+pokemonList.get(i).nombre);
-        pokemonViewHolder.pokemonTipo.setText("Tipo: "+pokemonList.get(i).tipo);
-        //entonces no cambiamos el color?
-        //pokemonViewHolder.color = getCardColor(pokemonList.get(i).tipo);
-    }
-
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
         recycler = recyclerView;
     }
 
-    public class PokemonViewHolder extends RecyclerView.ViewHolder {
-
-        TextView pokemonNombre;
-        TextView pokemonTipo;
-        ImageView pokemonFoto;
+    public class AnimeViewHolder extends RecyclerView.ViewHolder {
+        // Campos respectivos de un item
+        public ImageView imagen;
+        public TextView nombre;
+        public TextView tipo;
         public View view;
 
-        PokemonViewHolder(View itemView) {
-            super(itemView);
-            view = itemView;
-            pokemonNombre = (TextView)itemView.findViewById(R.id.nombre);
-            pokemonTipo = (TextView)itemView.findViewById(R.id.tipo);
-            pokemonFoto = (ImageView)itemView.findViewById(R.id.imagen);
+        public AnimeViewHolder(View v) {
+            super(v);
+            view = v;
+            imagen = (ImageView) v.findViewById(R.id.imagen);
+            nombre = (TextView) v.findViewById(R.id.nombre);
+            tipo = (TextView) v.findViewById(R.id.tipo);
 
             //Cuando se hace click en una tarjeta
             view.setOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View v) {
                     int pos = recycler.getChildLayoutPosition(v);
-                    Toast.makeText(v.getContext(),"Pokemon clickado: "+ pokemonList.get(pos).id,Toast.LENGTH_LONG).show();
+                    Toast.makeText(v.getContext(),"Pokemon clickado: "+ items.get(pos).id,Toast.LENGTH_LONG).show();
                 }
             });
-            //cv.setCardBackgroundColor(color);
         }
+    }
+
+    public RVAdapter(List<Pokemon> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public AnimeViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.card_view, viewGroup, false);
+        return new AnimeViewHolder(v);
+    }
+
+    //Colocamos todos los datos de una tarjeta
+    @Override
+    public void onBindViewHolder(AnimeViewHolder viewHolder, int i) {
+        Glide.with(viewHolder.imagen.getContext())
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+items.get(i).id+".png")
+                //.placeholder(R.mipmap.ic_launcher_round)
+                .into(viewHolder.imagen);
+
+        viewHolder.nombre.setText(items.get(i).id+".-"+items.get(i).nombre);
+        viewHolder.tipo.setText(items.get(i).tipo);
     }
 }
