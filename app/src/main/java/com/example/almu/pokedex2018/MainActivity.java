57b9f59038dbb40466deccde0f078abf75c22620
gonzaf21 <gonzaf21@users.noreply.github.com>
2019-01-11 18:17:10 +0100

    Deshechos los cambios :cry:

--- app/src/main/java/com/example/almu/pokedex2018/MainActivity.java
+++ app/src/main/java/com/example/almu/pokedex2018/MainActivity.java
@@ -5,12 +5,18 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,9 +26,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
+
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.CAMERA;
@@ -30,12 +37,16 @@ import static android.Manifest.permission.READ_CONTACTS;
 import static android.Manifest.permission.RECORD_AUDIO;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        PokemonDetailFragment.OnFragmentInteractionListener {
     private ArrayList<String> permissionsToRequest;
     private ArrayList<String> permissionsRejected = new ArrayList<>();
     private ArrayList<String> permissions = new ArrayList<>();
     private final static int ALL_PERMISSIONS_RESULT = 101;
-    ImageView imageView;
+    RecyclerView rv;
+    LinearLayoutManager llm;
+    SQLiteDatabase db;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,13 +91,108 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        imageView = findViewById(R.id.ivMain);
+        // RecyclerView y LinearLayoutManager
+        rv = findViewById(R.id.reciclador);
+        rv.setHasFixedSize(true);
 
-        if(imageView != null) {
-            GlideApp.with(this)
-                    .load("https://media1.tenor.com/images/e9d5a73338a616c080d116c3b39f6894/tenor.gif")
-                    .into(imageView);
+        llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
 
+        BDPokemon pokemons = new BDPokemon(this, "BDPokemon", null, 1);
+        db = pokemons.getWritableDatabase();
+
+        // Datos para las cardview en una lista que irá al adapter
+        // Cargamos la lista con Retrofit y la pokeApi
+        List<Pokemon> pokemonList = cargarDatos();
+
+        RVAdapter adapter = new RVAdapter(pokemonList);
+        rv.setAdapter(adapter);
+
+        // Recarga con gesto de lista de pokémon
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Esto se ejecuta cada vez que se realiza el gesto
+                MainActivity.Recarga r  = new MainActivity.Recarga();
+                r.execute();
+            }
+        });
+    }
+
+    private List<Pokemon> cargarDatos() {
+        List<Pokemon> pokemonList = new ArrayList<>();
+        String[] args = new String[]{};
+        Cursor c = db.rawQuery("SELECT * FROM pokemon ORDER BY id;", args);
+        if(c.moveToFirst()) {
+            // Recorremos el cursor hasta que no haya mas registros
+            do {
+                Integer id = c.getInt(0);
+                String nombre = c.getString(1);
+                double altura = c.getDouble(2);
+                double peso = c.getDouble(3);
+                String tipoString = c.getString(4);
+                String habilidadString = c.getString(5);
+                Integer oculto = c.getInt(6);
+                double latitud = c.getDouble(7);
+                double longitud = c.getDouble(8);
+
+                // Tipos
+                Tipo[] tipo;
+                if(tipoString.contains(";"))
+                {
+                    tipo = new Tipo[2];
+                    tipo[0] = new Tipo(new Contenido(tipoString.split(";")[0]));
+                    tipo[1] = new Tipo(new Contenido(tipoString.split(";")[1]));
+                }
+                else{
+                    tipo = new Tipo[1];
+                    tipo[0] = new Tipo(new Contenido(tipoString));
+                }
+
+                // Habilidades
+                Habilidad[] habilidades;
+                if(habilidadString.contains(";"))
+                {
+                    habilidades = new Habilidad[2];
+                    habilidades[0] = new Habilidad(
+                            new Contenido(habilidadString.split(";")[0]));
+                    habilidades[1] = new Habilidad(
+                            new Contenido(habilidadString.split(";")[1]));
+                }
+                else{
+                    habilidades = new Habilidad[1];
+                    habilidades[0] = new Habilidad(new Contenido(habilidadString));
+                }
+
+                Pokemon p = new Pokemon(id,nombre,altura,peso,tipo,habilidades,oculto,latitud,longitud);
+                pokemonList.add(p);
+
+            }while(c.moveToNext());
+        }
+
+        return pokemonList;
+    }
+
+    // AsyncTask que recarga la lista del recyclerview
+    private class Recarga extends AsyncTask<Void, Void, List<Pokemon>> {
+
+        @Override
+        protected List<Pokemon> doInBackground(Void... params) {
+            /*try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+
+            return cargarDatos();
+        }
+
+        @Override
+        protected void onPostExecute(List<Pokemon> list) {
+            RVAdapter adapter = new RVAdapter(list);
+            rv.setAdapter(adapter);
+            swipeRefreshLayout.setRefreshing(false);
         }
     }
 
@@ -155,8 +261,15 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            moveTaskToBack(true);
+            if (getSupportFragmentManager().getBackStackEntryCount() > 0)
+            {
+                getSupportFragmentManager().popBackStack();
+                rv.setVisibility(View.VISIBLE);
+                return;
+            }
         }
+        moveTaskToBack(true);
+        //super.onBackPressed();
     }
 
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
@@ -194,9 +307,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
 
-        } else if (id == R.id.nav_list) {
-            Intent intent = new Intent(this, ListScreen.class);
-            startActivity(intent);
         } else if (id == R.id.nav_capture) {
             Intent intent = new Intent(this, CapturePokemon.class);
             startActivity(intent);
@@ -259,5 +369,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
+        // Cuando se vuelva a esta pantalla, se recargará la lista automáticamente.
+        Recarga r  = new Recarga();
+        r.execute();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
