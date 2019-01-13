627e81b791921bd362b9282b1a25da6a159f337d
AlmuChan <39128933+AlmuChan@users.noreply.github.com>
2019-01-13 22:44:38 +0100

    Ahora se puede guardar los pokemon que tienes capturados en tu cuenta!!

--- app/src/main/java/com/example/almu/pokedex2018/CapturePokemon.java
+++ app/src/main/java/com/example/almu/pokedex2018/CapturePokemon.java
@@ -1,11 +1,13 @@
 package com.example.almu.pokedex2018;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +16,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 public class CapturePokemon extends AppCompatActivity
@@ -26,6 +37,8 @@ public class CapturePokemon extends AppCompatActivity
     private String urlGif = "https://cdn57.androidauthority.net/wp-content/uploads/2015/05/" +
             "Chop-Twice-for-flashlight.gif";
     private LinearLayout linearLayout;
+    int numC;
+    String usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +74,16 @@ public class CapturePokemon extends AppCompatActivity
 
                 Toast.makeText(CapturePokemon.this, "¡Pokémon capturado!",
                         Toast.LENGTH_SHORT).show();
+
+                // Coge usuario de preferencias
+                SharedPreferences preferences = getSharedPreferences("myprefs", MODE_PRIVATE);
+                String user = preferences.getString("user", "");
+
+                numC = numCapturado;
+                usuario = user;
+
+                ObtenerPokemonsCapturados login = new ObtenerPokemonsCapturados();
+                login.execute(user);
 
                 // Sonido al capturar
                 SharedPreferences pref =
@@ -116,5 +139,113 @@ public class CapturePokemon extends AppCompatActivity
             return;
         }
         super.onBackPressed();
+    }
+
+    public class ActualizarPokes extends AsyncTask<String,Void,Void> {
+
+
+        @Override
+        protected Void doInBackground(String... strings) {
+
+            try {
+                HttpURLConnection urlConn;
+                URL url = new URL("http://almudenalopezsanchez.000webhostapp.com/actualizarPokemons.php");
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setDoInput(true);
+                urlConn.setDoOutput(true);
+                urlConn.setUseCaches(false);
+                urlConn.setRequestProperty("Content-Type", "application/json");
+                urlConn.setRequestProperty("Accept", "application/json");
+                urlConn.connect();
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("pokesCapturados", strings[0]);
+                jsonParam.put("usuario", strings[1]);
+
+                OutputStream os = urlConn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(jsonParam.toString());
+                writer.flush();
+                writer.close();
+
+                int respuesta = urlConn.getResponseCode();
+
+                StringBuilder result = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        result.append(line);
+                    }
+                    br.close();
+
+                }
+            }
+            catch (Exception e) {
+            }
+            return null;
+        }
+    }
+
+    public class ObtenerPokemonsCapturados extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            // Actualiza pokemon en usuario db
+            ActualizarPokes actualizarPokes = new ActualizarPokes();
+            actualizarPokes.execute(s + ";" + numC, usuario);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                HttpURLConnection urlConn;
+                URL url = new URL("http://almudenalopezsanchez.000webhostapp.com/obtenerPokesCapturados.php");
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setDoInput(true);
+                urlConn.setDoOutput(true);
+                urlConn.setUseCaches(false);
+                urlConn.setRequestProperty("Content-Type", "application/json");
+                urlConn.setRequestProperty("Accept", "application/json");
+                urlConn.connect();
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("usuario", strings[0]);
+
+                OutputStream os = urlConn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(jsonParam.toString());
+                writer.flush();
+                writer.close();
+
+                int respuesta = urlConn.getResponseCode();
+
+                StringBuilder result = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        result.append(line);
+                    }
+                    br.close();
+
+                    JSONObject respuestaJSON = new JSONObject(result.toString());
+
+                    JSONObject resultJSON = respuestaJSON.getJSONObject("pokesCapturados");
+
+                    return resultJSON.getString("pokesCapturados");
+
+                }
+            }
+            catch (Exception e) {
+            }
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/almu/pokedex2018/LoginActivity.java
+++ app/src/main/java/com/example/almu/pokedex2018/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.almu.pokedex2018;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -68,8 +72,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 editor.putString("user", txtUsuario.getText().toString());
                 editor.commit();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                ObtenerPokemonsCapturados servicio = new ObtenerPokemonsCapturados();
+                servicio.execute(txtUsuario.getText().toString());
             }
             else
             {
@@ -119,6 +123,83 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     String resultJSON = respuestaJSON.getString("estado");
 
                     return resultJSON;
+                }
+            }
+            catch (Exception e) {
+            }
+            return null;
+        }
+    }
+
+    public class ObtenerPokemonsCapturados extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            BDPokemon pokemons = new BDPokemon(LoginActivity.this, "BDPokemon", null, 1);
+            SQLiteDatabase db = pokemons.getWritableDatabase();
+
+            for(int i = 1; i <152; i++)
+            {
+                db.execSQL("UPDATE pokemon SET oculto = 0 WHERE id = " + i + ";");
+            }
+
+            if(!s.equals(""))
+            {
+                String[] pokes = s.split(";");
+                for(int i = 0; i < pokes.length; i++)
+                {
+                    db.execSQL("UPDATE pokemon SET oculto = 1 WHERE id = " + pokes[i] + ";");
+                }
+            }
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                HttpURLConnection urlConn;
+                URL url = new URL("http://almudenalopezsanchez.000webhostapp.com/obtenerPokesCapturados.php");
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setDoInput(true);
+                urlConn.setDoOutput(true);
+                urlConn.setUseCaches(false);
+                urlConn.setRequestProperty("Content-Type", "application/json");
+                urlConn.setRequestProperty("Accept", "application/json");
+                urlConn.connect();
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("usuario", strings[0]);
+
+                OutputStream os = urlConn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(jsonParam.toString());
+                writer.flush();
+                writer.close();
+
+                int respuesta = urlConn.getResponseCode();
+
+                StringBuilder result = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        result.append(line);
+                    }
+                    br.close();
+
+                    JSONObject respuestaJSON = new JSONObject(result.toString());
+
+                    JSONObject resultJSON = respuestaJSON.getJSONObject("pokesCapturados");
+
+                    return resultJSON.getString("pokesCapturados");
+
                 }
             }
             catch (Exception e) {
